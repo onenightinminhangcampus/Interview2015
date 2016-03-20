b324c65862f63cb8f4303442247bc87f1f052f31
sjtupuzhao <onenightinminhangcampus@users.noreply.github.com>
2016-03-21 02:28:56 +0800

    wap

---
+++ WAP-Exam1_C++.cc
@@ -0,0 +1,256 @@
+﻿//Game
+//Jeff loves playing games, Gluttonous snake(an old game in NOKIA era) is one of his favourites.However, after playing gluttonous snake so many times, he finally got bored with the original rules.
+//
+//In order to bring new challenge to this old game, Jeff introduced new rules :
+//
+//The ground is a grid, with n rows and m columns(1 <= n, m <= 500).
+//Each cell contains a value v(-1 ≤ vi≤  99999), if v is - 1, then this cell is blocked, and the snake can not go through, otherwise, after the snake visited this cell, you can get v point.
+//The snake can start from any cell along the left border of this ground and travel until it finally stops at one cell in the right border.
+//During this trip, the snake can only go up / down / right, and can visit each cell only once.
+//Special cases :
+//
+//a.Even in the left border and right border, the snake can go up and down b.When the snake is at the top cell of one column, it can still go up, which demands the player to pay all current points, then the snake will be teleported to the bottom cell of this column and vice versa.
+//
+//After creating such a new game, Jeff is confused how to get the highest score.Please help him to write a program to solve this problem.
+//
+//Input
+//
+//The first line contains two integers n(rows) and m(columns), (1 <= n, m <= 500), separated by a single space.
+//
+//Next n lines describe the grid.Each line contains m integers vi(-1 ≤ vi ≤ 99999) vi = -1 means the cell is blocked.
+//
+//Output
+//
+//Output the highest score you can get.If the snake can not reach the right side, output - 1.
+//
+//Limits
+//
+//Memory limit per test : 256 megabytes
+//Time limit per test : The faster the better
+//Compile & Environment
+//
+//C++
+//
+//g++ Main.cc - o Main - fno - asm - Wall - lm--static - std = c++0x - DONLINE_JUDGE
+//
+//Input
+//4 4
+//- 1 4 5 1
+//2 - 1 2 4
+//3 3 - 1 3
+//4 2 1 2
+//output
+//23
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> matrix(n, vector<int>(m));
+	for (int i = 0; i < n;i++)
+	{
+		for (int j = 0; j < m;j++)
+		{
+			int val;
+			cin >> val;
+			matrix[i][j] = val;
+		}
+	}
+
+	//int n = 4, m = 4;
+	//vector<vector<int>> matrix(n, vector<int>(m));
+	//int i = 0, j = 0;
+	//matrix[i][j] = -1;
+	//j++;
+	//matrix[i][j] = 4;
+	//j++;
+	//matrix[i][j] = 5;
+	//j++;
+	//matrix[i][j] = 1;
+
+	//j = 0; i++;
+	//matrix[i][j] = 2;
+	//j++;
+	//matrix[i][j] = -1;
+	//j++;
+	//matrix[i][j] = 2;
+	//j++;
+	//matrix[i][j] = 4;
+
+	//j = 0; i++;
+	//matrix[i][j] = 3;
+	//j++;
+	//matrix[i][j] = 3;
+	//j++;
+	//matrix[i][j] = -1;
+	//j++;
+	//matrix[i][j] = 3;
+
+	//j = 0; i++;
+	//matrix[i][j] = 4;
+	//j++;
+	//matrix[i][j] = 2;
+	//j++;
+	//matrix[i][j] = 1;
+	//j++;
+	//matrix[i][j] = 2;
+
+	vector<vector<int>> values(n, vector<int>(m + 1));
+	//Increment the values based on col
+	//the initial column of values has all 0
+	for (int i = 0; i < n;i++)
+	{
+		values[i][0] = 0;
+	}
+
+
+	vector<int> tmp(n);
+	vector<int> ini(n);
+	//traverse through all columns
+	for (int col = 1; col < m + 1;col++)
+	{
+		//moving right
+		//get the initial value of this col
+		for (int row = 0; row < n;row++)
+		{
+			if (matrix[row][col-1]==-1)
+			{
+				values[row][col] = -1;
+			}
+			else if (values[row][col-1]==-1)
+			{
+				values[row][col] = matrix[row][col - 1];
+			}
+			else
+			{
+				values[row][col] = values[row][col - 1] + matrix[row][col - 1];
+			}
+			ini[row] = values[row][col];
+			tmp[row] = values[row][col];
+		}
+		//-1 2 3 4
+
+		//Then operate on the initial value values[row][col]
+		//path on a column can only be pointing up or pointing down
+
+		//Every entry going up/down generate a unique tmp col based on the same initial value values[row][col]
+		//Get the largest of each entry and store in tmp[row], assign it to values[row][col] in the end
+
+		//moving down
+		//Every step is supposing that we start from one entry and going down until -1 or return to the original entry after teleport
+		for (int row = 0; row < n;row++)
+		{
+			//row=1
+			//tmp: -1 2 3 4
+			for (int tmp_row = 0; tmp_row < n; tmp_row++)
+			{
+				tmp[tmp_row] = matrix[tmp_row][col-1];
+			}
+			tmp[row] = ini[row];
+
+			if (tmp[row]==-1)
+			{
+				continue;
+			}
+
+			//last=1
+			int last = row;
+			for (int ind = row+1;;ind++)
+			{
+				//ind=2
+				//ind=3
+				//ind=0
+				ind = (ind + n) % n;
+
+				if (tmp[ind]==-1||ind==row)
+				{
+					break;
+				}
+
+				//teleported, need to pay all of the points
+				if (abs(ind-last)==1)
+				{
+					tmp[ind] += tmp[last];
+				}
+				//tmp: -1 2 5 9
+
+				last = ind;
+				//last=2
+				//last=3
+			}
+
+
+			for (int tmp_row = 0; tmp_row < n; tmp_row++)
+			{
+				if (tmp[tmp_row]>values[tmp_row][col])
+				{
+					values[tmp_row][col] = tmp[tmp_row];
+				}
+			}
+
+		}
+
+
+		//moving up
+		for (int row = 0; row < n; row++)
+		{
+			for (int tmp_row = 0; tmp_row < n; tmp_row++)
+			{
+				tmp[tmp_row] = matrix[tmp_row][col - 1];
+			}
+			tmp[row] = ini[row];
+
+			if (tmp[row] == -1)
+			{
+				continue;
+			}
+
+			int last = row;
+			for (int ind = row - 1;; ind--)
+			{
+				ind = (ind + n) % n;
+
+				if (tmp[ind] == -1 || ind == row)
+				{
+					break;
+				}
+
+				//teleported, need to pay all of the points
+				if (abs(ind - last) == 1)
+				{
+					tmp[ind] += tmp[last];
+				}
+
+				last = ind;
+
+			}
+
+
+			for (int tmp_row = 0; tmp_row < n; tmp_row++)
+			{
+				if (tmp[tmp_row]>values[tmp_row][col])
+				{
+					values[tmp_row][col] = tmp[tmp_row];
+				}
+			}
+
+		}
+
+
+	}
+
+	int largest = 0;
+	for (int row = 0; row < n;row++)
+	{
+		largest = max(largest, values[row][m]);
+	}
+
+	cout << largest << endl;
+	//cin.get();
+
+}
---
+++ WAP-Exam2_C++.cc
@@ -0,0 +1,160 @@
+﻿//Travel Information Center
+//Aps Island has many cities.In the summer, many travellers will come to the island and attend festive events in different cities.The festive events in Aps Island are crazy.Once it starts, it will never end.In the following sentences, the cities which have festive events are called festive cities.
+//
+//At the beginning, only city No. 1 is festive city.If a new city becomes festive city, the government will tell the information center about this news.
+//
+//Everyday, the information center will receive many inquiries from travellers from different cities of this land.They want to know the closest festive city, and calculate the distance(If current city has festive event, the distance is 0).
+//
+//Due to the growing number of the travellers, the information center is overloaded.The government wants to fix the problem by developing a system to handle the inquiries automatically.
+//
+//As a fact, cities in Aps Island are connected with highways(bidirectional, length of every highway is 1).Any two cities are connected directly or indirectly, and there is ONLY one path between any 2 cities.
+//
+//Input
+//
+//There are two integers in the first line, n(2 ≤ n ≤ 10 ^ 5) and m(1 ≤ m ≤ 10 ^ 5), n is the number of cities in the Aps Island and m is the number of queries.The coming n - 1 lines are the highways which connect two cities.In the line, there are two integers ai and bi(1 ≤ ai, bi ≤ n, ai ≠ bi), representing two cities.Each line means the highway connecting the two cities.
+//
+//Next m lines are inquiries from travellers or news from government.Each line has two integers qi and ci(1 ≤ qi ≤ 2, 1 ≤ ci ≤ n).If qi  = 1, the government announces a new festive city ci.If qi  = 2, you have to find and print the shortest distance from the city ci to the closest festive city.
+//
+//Output
+//
+//Results from each(qi = 2) Questions.Print every result with a new line.
+//
+//Limits
+//
+//Memory limit per test : 256 megabytes
+//Time limit per test : The faster the better
+//Compile & Environment
+//
+//C++
+//
+//g++ Main.cc - o Main - fno - asm - Wall - lm--static - std = c++0x - DONLINE_JUDGE
+
+//Input
+//n(2 ≤ n ≤ 10 ^ 5) and m(1 ≤ m ≤ 10 ^ 5)
+//5 5
+//n=5, m=5
+
+//1 2
+//1 3
+//3 4
+//3 5
+//2 5
+//2 3
+//1 3
+//2 3
+//2 4
+
+//2
+//1
+//0
+//1
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+namespace Cities
+{
+	struct City
+	{
+		//festive flag
+		int festive=0;
+		vector<int> neighbours;
+	};
+
+	struct Connects
+	{
+		//current node
+		int cur;
+		//parent node
+		int par;
+		//acccumulated distance
+		int dis;
+	};
+}
+
+using namespace Cities;
+
+int bfs(vector<City> all, int c, int n);
+
+int main()
+{
+	int n, m, a, b;
+	cin >> n >> m;
+	//Initialize a list of cities
+	vector<City> allCities(n);
+
+	allCities[0].festive = 1;
+	int num = n;
+	while (--num)
+	{
+		//City a and city b are connected
+		cin >> a >> b;
+		a--, b--;
+		allCities[a].neighbours.push_back(b);
+		allCities[b].neighbours.push_back(a);
+	}
+
+	int q, c, dis;
+	while (m)
+	{
+		cin >> q>>c;
+		c--;
+		//q=1, government anounce a city
+		if (q==1)
+		{
+			allCities[c].festive = 1;
+		}
+
+		//q=2, search for the nearst festive city
+		if (q==2)
+		{
+			dis = bfs(allCities, c, n);
+			printf("%d\n", dis);
+		}
+
+		m--;
+	}
+}
+
+int bfs(vector<City> all, int c, int n)
+{
+	queue<Connects> que;
+	//bfs search start from the root which is provide by c
+	Connects root;
+	root.cur = c;
+	root.dis = 0;
+
+	que.push(root);
+	while (!que.empty())
+	{
+		//pop up a connect
+		Connects now = que.front();
+		que.pop();
+		//
+		if (all[now.cur].festive)
+		{
+			return now.dis;
+		}
+		//traverse through all of the connected nodes
+		int search = all[now.cur].neighbours.size();
+		while (search)
+		{
+			search--;
+			//make sure the traverse does not go to the current node's parent node
+			if (all[now.cur].neighbours[search]==now.par)
+			{
+				continue;
+			}
+			Connects push;
+			push.par = now.cur;
+			push.cur = all[now.cur].neighbours[search];
+			push.dis = now.dis + 1;
+
+			que.push(push);
+			
+		}
+
+	}
+
+}
